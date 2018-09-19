62b18bf50c158d40e5563332c4a066713f119562
王一川 <kingjames6236@163.com>
2018-09-19 18:45:16 +0800

    update

---
+++ config-client/src/main/java/com/rivers/configclient/controller/testController.java
@@ -0,0 +1,19 @@
+package com.rivers.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class testController {
+
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return this.profile;
+    }
+}

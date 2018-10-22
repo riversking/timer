cc618dd67ddcad3a27cf6459454ebfbd5da220c9
riversking <kingjames6236@163.com>
2018-10-23 00:17:51 +0800

    update

--- eureka-admin/src/main/java/com/riversking/eurekaadmin/EurekaAdminApplication.java
+++ eureka-admin/src/main/java/com/riversking/eurekaadmin/EurekaAdminApplication.java
@@ -2,12 +2,15 @@ package com.riversking.eurekaadmin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
 
 @EnableEurekaClient
 @EnableAdminServer
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 public class EurekaAdminApplication {
 
     public static void main(String[] args) {

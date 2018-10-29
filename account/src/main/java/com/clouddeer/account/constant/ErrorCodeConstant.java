bb4581a89d518f9c27d2c89e1dc00ce5848468f7
wangyichuan <kingjames6236@163.com>
2018-10-29 13:35:01 +0800

    update jar

--- account/src/main/java/com/clouddeer/account/constant/ErrorCodeConstant.java
+++ account/src/main/java/com/clouddeer/account/constant/ErrorCodeConstant.java
@@ -7,14 +7,22 @@ package com.clouddeer.account.constant;
  */
 public class ErrorCodeConstant {
 
-    /**服务器错误*/
+    /**
+     * 服务器错误
+     */
     public static final String ECODE_0000 = "500";
 
-    /**查询账号失败*/
-    public static final String ECODE_0301="1060301";
-    /**新增账号失败*/
-    public static final String ECODE_0302="1060302";
-    /**保存验证码失败*/
-    public static final String ECODE_0303="1060302";
+    /**
+     * 查询账号失败
+     */
+    public static final String ECODE_0301 = "1060301";
+    /**
+     * 新增账号失败
+     */
+    public static final String ECODE_0302 = "1060302";
+    /**
+     * 保存验证码失败
+     */
+    public static final String ECODE_0303 = "1060302";
 
 }

a8261ace7adea9ea68a1d3a541173fcc9676b863
wangyichuan <kingjames6236@163.com>
2018-11-02 10:11:26 +0800

    update

--- oauth/src/main/java/com/riversking/oauth/config/WebSecurityConfigurer.java
+++ oauth/src/main/java/com/riversking/oauth/config/WebSecurityConfigurer.java
@@ -1,16 +1,16 @@
 package com.riversking.oauth.config;
 
-import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-
+@EnableWebSecurity
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
 
 

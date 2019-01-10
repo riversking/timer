771f6d896198f4ca501f686863412093093755f3
wangyichuan <kingjames6236@163.com>
2019-01-10 11:12:22 +0800

    update gradle 配置

---
+++ user-api/src/main/java/com/rivers/user/api/utils/UserUtils.java
@@ -0,0 +1,15 @@
+package com.rivers.user.api.utils;
+
+public class UserUtils {
+
+
+    public static void main(String[] ars) {
+        try {
+            System.out.print("2222222");
+        } catch (Exception e) {
+            System.out.print("11111");
+            System.out.print(e.getMessage());
+        }
+
+    }
+}

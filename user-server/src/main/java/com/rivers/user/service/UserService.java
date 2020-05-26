0957249f4bdf7d298aa358fb0b359f9c4f0aeb02
riversking <1325010917@qq.com>
2020-05-26 22:43:09 +0800

    update gradle

--- user-server/src/main/java/com/rivers/user/service/UserService.java
+++ user-server/src/main/java/com/rivers/user/service/UserService.java
@@ -6,6 +6,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.demo.A;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.rivers.core.util.ExceptionUtil;

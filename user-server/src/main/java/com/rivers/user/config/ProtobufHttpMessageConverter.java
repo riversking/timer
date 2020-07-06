10a09f60c6e6be798424a6bac0c7e148aec8952b
riversking <1325010917@qq.com>
2020-07-06 11:15:00 +0800

    update config

--- user-server/src/main/java/com/rivers/user/config/ProtobufHttpMessageConverter.java
+++ user-server/src/main/java/com/rivers/user/config/ProtobufHttpMessageConverter.java
@@ -26,7 +26,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
-
+@Configuration
 public class ProtobufHttpMessageConverter extends AbstractHttpMessageConverter<Message> {
 
 

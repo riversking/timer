36406fd597bb80cdd67b7b2e6caf99e28e3e73a6
riversking <1325010917@qq.com>
2020-07-07 19:56:23 +0800

    fix config

--- file-server/src/main/java/com/rivers/file/client/UserClientFeign.java
+++ file-server/src/main/java/com/rivers/file/client/UserClientFeign.java
@@ -1,5 +1,7 @@
 package com.rivers.file.client;
 
+import com.rivers.core.config.ProtobufHttpMessageConverter;
+import com.rivers.core.config.RequestInterceptorConfig;
 import com.rivers.userservice.proto.GetUserListReq;
 import com.rivers.userservice.proto.GetUserListRes;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 /**
  * @author riversking
  */
-@FeignClient(name = "USER-SERVER")
+@FeignClient(name = "USER-SERVER", configuration = {RequestInterceptorConfig.class, ProtobufHttpMessageConverter.class})
 public interface UserClientFeign {
 
 

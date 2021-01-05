95baf37e3a46e8515732ff182cbecd0ab3318075
riversking <kingjames6236@163.com>
2021-01-05 17:21:25 +0800

    oauth

--- oauth-server/src/main/java/com/rivers/oauth/common/CustomWebResponseExceptionTranslator.java
+++ oauth-server/src/main/java/com/rivers/oauth/common/CustomWebResponseExceptionTranslator.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 @Component("customWebResponseExceptionTranslator")
 public class CustomWebResponseExceptionTranslator implements WebResponseExceptionTranslator {
     @Override
-    public ResponseEntity<OAuth2Exception> translate(Exception e) throws Exception {
+    public ResponseEntity<OAuth2Exception> translate(Exception e) {
 
         OAuth2Exception oAuth2Exception = (OAuth2Exception) e;
         return ResponseEntity
